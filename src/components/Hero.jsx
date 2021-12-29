64b7aef3cc25a02ea229be72abe8aaa85e39b6ab
Laitas <marciukash@gmail.com>
2021-12-29 20:09:44 +0200

    Created Hero styling

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from './Button'
+import { useNavigate } from 'react-router-dom'
+
+const Hero = () => {
+    const navigate = useNavigate()
+
+    return(
+
+        <header className='bg-black flex justify-center'>
+        <div className="md:w-11/12 bg-no-repeat bg-center bg-cover md:bg-right bg-headphonesM sm:bg-headphonesTab md:bg-headphonesL">
+        <div className="md:w-1/2 text-center md:text-left px-24 md:px-0 h-600 flex flex-col justify-center p-4 ">
+        <h2 className='text-gray-500 uppercase tracking-[0.75em]'>New product</h2>
+        <h1 className='text-6xl uppercase text-white py-8'>XX99 MARK II Headphones</h1>
+        <p className='text-white'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+        <div className='my-8'>
+        <Button onClick={()=> navigate('/xx99-mark-two-headphones')} text="See product"/>
+        </div>
+        </div>
+        </div>
+    </header>
+        )
+}
+
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'headphonesL': "linear-gradient(to right, rgba(0,0,0,0.9), rgba(0,0,0,0.45),rgba(0,0,0,0.9)), url('./assets/home/desktop/image-hero.jpg')",
+        'headphonesTab': "linear-gradient(to right, rgba(0,0,0,0.9), rgba(0,0,0,0.45),rgba(0,0,0,0.9)), url('./assets/home/tablet/image-header.jpg')",
+        'headphonesM': "linear-gradient(to right, rgba(0,0,0,0.7), rgba(0,0,0,0.35),rgba(0,0,0,0.7)), url('./assets/home/mobile/image-header.jpg')",
+      },
+      height: {
+        '600': '40rem',
+      },
       colors : {
         'orange' : '#D87D4A',
         'light-orange' : '#fbaf85',
         'custom-gray' : '#f1f1f1',
-        'custom-light-gray' : '#fafafa'
+        'custom-light-gray' : '#fafafa',
       }
     },
   },
